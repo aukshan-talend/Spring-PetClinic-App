3c8b5e233f892a49f034f071d7b3eec599faadee
aukshan-talend <ajoshi@talend.com>
2021-11-25 11:12:15 +0530

    Initial Project Structure

---
+++ src/main/java/com/petclinic/springpetclinicapp/SpringPetClinicAppApplication.java
@@ -0,0 +1,13 @@
+package com.petclinic.springpetclinicapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPetClinicAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPetClinicAppApplication.class, args);
+    }
+
+}
